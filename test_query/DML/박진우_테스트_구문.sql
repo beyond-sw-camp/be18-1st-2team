5ddc382e659a96f212df32267f335c5a2cf604b6
DESKTOP-GIU52QC\JINWOO <pjw1480@naver.com>
2025-07-07 22:42:17 +0900

    [박진우]  데이터 무결성 검증 테스트 샘플

--- test_query/DML/박진우_테스트_구문.sql
+++ test_query/DML/박진우_테스트_구문.sql
@@ -28,6 +28,42 @@ ALTER TABLE `user`
 ADD CONSTRAINT fk_user_team_id FOREIGN KEY(team_id)
 REFERENCES team(team_id);
 
+-- 시즌 도중 염경엽 감독이 해임되고 김정준 수석코치가 감독으로 임명되었다면
+UPDATE staff
+SET resignation_date = '2023-06-30'
+WHERE staff_name = '염경엽' AND role = '감독' AND team_id = 1;
+
+UPDATE staff
+SET resignation_date = '2023-06-30'
+WHERE staff_name = '김정준' AND role = '수석코치' AND team_id = 1;
+
+INSERT INTO staff (staff_id, team_id, role, staff_name, phone, career, hire_date, resignation_date)
+VALUES (21, 1, '감독', '김정준', '010-1111-0002', '15년', '2023-07-01', NULL);
+
+-- 김정준의 이력 조회
+SELECT *
+FROM staff
+WHERE staff_name = '김정준' AND team_id = 1
+ORDER BY hire_date;
+
+SELECT team_id, COUNT(*) AS num_directors
+FROM staff
+WHERE role = '감독'
+  AND resignation_date IS NULL
+GROUP BY team_id
+HAVING COUNT(*) > 1;
+
+
+
+--데이터 무결성 검증 쿼리문
+
+-- 퇴사일이 입사일 보다 빠르지 않은지 검증.
+SELECT *
+FROM staff
+WHERE resignation_date IS NOT NULL
+  AND resignation_date < hire_date;
+
+
 -- LG 트윈스의 시즌별 성적 확인
 SELECT r.season AS 시즌, r.game_count AS 경기수, r.win_count AS 승, r.draw_count AS 무, r.lose_count AS 패, r.winning_per AS 승률
 FROM team_record r
