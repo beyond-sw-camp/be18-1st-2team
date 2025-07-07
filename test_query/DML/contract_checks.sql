70f4153e865d3399ecb1393d2f21378077c08996
promandu <clsrn2376@naver.com>
2025-07-07 17:53:32 +0900

    [김민준] 훈련,계약 검증항목

--- test_query/DML/contract_checks.sql
+++ test_query/DML/contract_checks.sql
@@ -1,18 +1,3 @@
--- contract 테이블 player_id 외래키 추가 
-ALTER TABLE `contract`
-ADD CONSTRAINT `FK_ct_player_id`
-FOREIGN KEY (`player_id`) REFERENCES `player`(`player_id`);
-
--- contract 테이블에 team_id 외래키 추가
-ALTER TABLE `contract`
-ADD CONSTRAINT `FK_ct_team_id`
-FOREIGN KEY (`team_id`) REFERENCES `team`(`team_id`);
-
--- train 테이블에 player_id 외래키 추가
-ALTER TABLE `train`
-ADD CONSTRAINT `FK_tr_player_id`
-FOREIGN KEY (`player_id`) REFERENCES `player`(`player_id`);
-
 -- 계약 시작일과 종료일 검증
 ALTER TABLE contract 
 ADD CONSTRAINT chk_contract_dates CHECK (contract_start < contract_end);
