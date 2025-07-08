48abfb95a2a5d0b4347efd7ae9ecaffd6819b6bd
IIIjong <pjwfish@naver.com>
2025-07-08 16:41:35 +0900

    [박종원] 테이블 생성문 데이터 유형 길이 변경

--- test_query/DDL/테이블 일괄 유니크 제약조건 포함.sql
+++ test_query/DDL/테이블 일괄 유니크 제약조건 포함.sql
@@ -167,17 +167,17 @@ CREATE TABLE `game_record_detail` (
 
 -- player_record 테이블 (선수 Season 기록)
 CREATE TABLE `player_record` (
-  `player_record_id` INT NOT NULL COMMENT '선수기록 ID',
+  `player_record_id` INT NOT NULL AUTO_INCREMENT COMMENT '선수기록 ID',
   `player_id` INT NOT NULL COMMENT '선수 ID',
   `season` YEAR COMMENT '시즌연도',
-  `era` DECIMAL(3,2) COMMENT '평균자책점',
-  `whip` DECIMAL(3,2) COMMENT 'WHIP',
+  `era` DECIMAL(4,2) COMMENT '평균자책점',
+  `whip` DECIMAL(4,2) COMMENT 'WHIP',
   `avg` DECIMAL(4,3) COMMENT '타율',
   `obp` DECIMAL(4,3) COMMENT '출루율',
   `slg` DECIMAL(4,3) COMMENT '장타율',
   `ops` DECIMAL(4,3) COMMENT 'OPS',
   `epct` DECIMAL(4,3) COMMENT '수비율',
-  `sba` DECIMAL(3,1) COMMENT '도루성공률',
+  `sba` DECIMAL(4,1) COMMENT '도루성공률',
   PRIMARY KEY (`player_record_id`),
   UNIQUE KEY (`player_id`,`season`), -- 선수의 동일 시즌 중복금지
   CONSTRAINT `FK_pr_player` FOREIGN KEY (`player_id`) REFERENCES `player` (`player_id`)
