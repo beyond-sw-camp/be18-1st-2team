46c07e14f6a4e46d729003910fc7acb9abd99ca8
xxiuan <156274066+xxiuan@users.noreply.github.com>
2025-07-08 14:12:14 +0900

    [윤석현] 경기 리드미 쿼리

---
+++ readme_query/games.sql
@@ -0,0 +1,33 @@
+
+-- 경기 등록 (새로운 경기 등록)
+INSERT INTO `game` (`game_id`,`home`,`away`,`start_time`,`end_time`,`win`,`lose`,`home_score`,`away_score`,`stadium`) 
+VALUES
+(5021, 5, 1, '2024-06-22 18:00:00', '2024-06-22 21:00:00', 5, 1, 7, 6, '광주기아챔피언스필드');
+
+
+-- 경기 수정 (home이 7점, away가 10점, win에는 이긴 팀 id, lose에는 진 팀 id 수정)
+UPDATE game
+SET 
+  home_score = 7,
+  away_score = 10,
+  win = 1,         
+  lose = 5
+WHERE game_id = 1001;
+
+-- 경기 리스트 조회
+SELECT  
+  ht.team_name AS '홈팀',
+  at.team_name AS '어웨이팀',
+   DATE_FORMAT(g.start_time, '%Y년 %m월 %d일') AS '경기일자',
+   wt.team_name AS '승리팀',
+  g.home_score AS '홈팀 점수',
+  g.away_score AS '어웨이팀 점수',
+  g.stadium AS '경기장'
+FROM game g
+LEFT JOIN team ht ON g.home = ht.team_id
+LEFT JOIN team at ON g.away = at.team_id
+LEFT JOIN team wt ON g.home = wt.team_id;
+
+-- 경기 삭제
+DELETE FROM game
+WHERE game_id = 1001
