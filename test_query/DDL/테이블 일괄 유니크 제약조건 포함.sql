c694a498b75a8ad3bc351f8e049206d3eb6c5ee6
IIIjong <pjwfish@naver.com>
2025-07-09 09:19:55 +0900

    [박종원] 테이블 최종

--- test_query/DDL/테이블 일괄 유니크 제약조건 포함.sql
+++ test_query/DDL/테이블 일괄 유니크 제약조건 포함.sql
@@ -30,8 +30,10 @@ CREATE TABLE `user` (
   `email` VARCHAR(30) COMMENT '이메일',
   `permission_id` INT NOT NULL COMMENT '권한 ID',
   `team_id` INT NOT NULL COMMENT '팀 ID',
+  `resident_number` CHAR(14) NOT NULL COMMENT '주민 번호',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY (`email`), -- 이메일 중복금지
+  UNIQUE KEY `unique_resident_number` (`resident_number`), -- 주민번호 중복금지
   CONSTRAINT `FK_user_permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`),
   CONSTRAINT `FK_user_team`       FOREIGN KEY (`team_id`)       REFERENCES `team` (`team_id`)
 );
